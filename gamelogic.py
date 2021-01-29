0ecc9fb7f67d55d11ee552f69c0a99709dcaa7cb
Bendegúz Szabó <szabobd95@gmail.com>
2021-01-29 14:45:53 +0100

    on_key_press refactored

--- gamelogic.py
+++ gamelogic.py
@@ -18,44 +18,45 @@ level = 1
 
 
 def on_key_press(e):
-    if not hero.is_alive:
-        canvas.create_rectangle(0, 720, 720, 820, fill='white')
-        canvas.create_text(350, 770, fill="black", font="Arial 14 bold",
-                           text="GAME OVER, YOU ARE DEAD, WASTED")
-        return
     is_tile_occupied = hero.is_tile_occupied(area.enemy_list)
-    if e.keycode == 65:
+    control_keys = [65, 68, 83, 87]
+
+    if not is_tile_occupied and e.keycode in control_keys:
         hero.erase(canvas, background, image_size)
-        if grid.grid[hero.x - 1][hero.y].cell_type == "floor" and hero.x - 1 >= 0 and not is_tile_occupied:
-            hero.x = hero.x - 1
-        hero.image = "hero_left"
+
+        if e.keycode == 65:
+            if grid.grid[hero.x - 1][hero.y].cell_type == "floor" and hero.x - 1 >= 0:
+                hero.x = hero.x - 1
+
+            hero.image = "hero_left"
+        elif e.keycode == 68:
+            if hero.x + 1 <= 9 and grid.grid[hero.x + 1][hero.y].cell_type == "floor":
+                hero.x = hero.x + 1
+
+            hero.image = "hero_right"
+        elif e.keycode == 83:
+            if hero.y + 1 <= 9 and grid.grid[hero.x][hero.y + 1].cell_type == "floor":
+                hero.y = hero.y + 1
+
+            hero.image = "hero_down"
+        elif e.keycode == 87:
+            if grid.grid[hero.x][hero.y - 1].cell_type == "floor" and hero.y - 1 >= 0:
+                hero.y = hero.y - 1
+
+            hero.image = "hero_up"
+
         hero.draw(canvas, background, image_size)
-    elif e.keycode == 68:
-        hero.erase(canvas, background, image_size)
-        if hero.x + 1 <= 9 and grid.grid[hero.x + 1][hero.y].cell_type == "floor" and not is_tile_occupied:
-            hero.x = hero.x + 1
-        hero.image = "hero_right"
-        hero.draw(canvas, background, image_size)
-    elif e.keycode == 83:
-        hero.erase(canvas, background, image_size)
-        if hero.y + 1 <= 9 and grid.grid[hero.x][hero.y + 1].cell_type == "floor" and not is_tile_occupied:
-            hero.y = hero.y + 1
-        hero.image = "hero_down"
-        hero.draw(canvas, background, image_size)
-    elif e.keycode == 87:
-        hero.erase(canvas, background, image_size)
-        if grid.grid[hero.x][hero.y - 1].cell_type == "floor" and hero.y - 1 >= 0 and not is_tile_occupied:
-            hero.y = hero.y - 1
-        hero.image = "hero_up"
-        hero.draw(canvas, background, image_size)
-    elif e.keycode == 32:
-        if is_tile_occupied:
-            current_enemy = hero.get_enemy_on_same_tile(area.enemy_list)
-            hero.strike(current_enemy, canvas, background, image_size, area)
-            if hero.killed_boss and hero.has_key:
-                area.level += 1
-                area.draw(grid, hero, canvas, background, image_size)
+
+    elif e.keycode == 32 and is_tile_occupied:
+        current_enemy = hero.get_enemy_on_same_tile(area.enemy_list)
+        hero.strike(current_enemy, canvas, background, image_size, area)
+
+        if hero.killed_boss and hero.has_key:
+            area.level += 1
+            area.draw(grid, hero, canvas, background, image_size)
+
     canvas.create_rectangle(0, 720, 720, 820, fill='white')
+
     if hero.is_tile_occupied(area.enemy_list):
         current_enemy = hero.get_enemy_on_same_tile(area.enemy_list)
         current_enemy.get_stats(canvas)
